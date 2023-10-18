34fdeb5ca0f4fcf33065566f88bfc0be278aee56
Нодир Асомов <nasomov93@gmail.com>
2023-10-19 00:07:51 +0500

    hi

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first>second){
+            System.out.println(first);
+        }
+        if (first<second){
+            System.out.println(second);
+        }else {
+            System.out.println(first+ "\n"+ second);
+        }
+    }
+
+    public static void main(String[] args) {
+        GreatestNumberPrinter obj = new GreatestNumberPrinter();
+        obj.printGreatest(2,8);
     }
 }
